0955c40a6d99f93b64d12f040393af6091d52654
Kylian Kinnen <0231733556@uni.lu>
2025-12-04 16:04:16 +0100

    added test suite attempt

--- letzhist/jest.config.js
+++ letzhist/jest.config.js
@@ -1,6 +1,7 @@
 const nextJest = require('next/jest')
 const createJestConfig = nextJest({ dir: './' })
 
+/** @type {import('jest').Config} */
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jsdom',
---
+++ letzhist/src/app/api/__test__/admin.test.ts
@@ -0,0 +1,5 @@
+describe('API: /api/admin (placeholders)', () => {
+  it.todo('PATCH /api/admin/users/:id/role updates user role')
+  it.todo('POST /api/admin/ip-bans creates an IP ban')
+  it.todo('DELETE /api/admin/ip-bans/:ip removes an IP ban')
+})
